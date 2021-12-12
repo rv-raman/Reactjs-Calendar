2671dd903c3c10c41bdf86ff9bb6a294471925ea
Venkata Raman <rsvr000@gmail.com>
2021-12-12 18:46:49 +0530

    react calendar

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './App.css';
+
+function App() {
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <div className='app'>
+      <h1 className='text-center'>React Calendar</h1>
+      <div className='calendar-container'>
+      <Calendar
+        onChange={setDate}
+        value={date}
+        maxDate={new Date(2031, 12, 12)} 
+        minDate={new Date(2011, 12, 12)} 
+        
+      />
+      </div>
+      {date.length > 0 ? (
+        <p className='text-center'>
+          <span className='bold'>Start:</span>{' '}
+          {date[0].toDateString()}
+          &nbsp;|&nbsp;
+          <span className='bold'>End:</span> {date[1].toDateString()}
+        </p>
+      ) : (
+        <p className='text-center'>
+          <span className='bold'>Default selected date:</span>{' '}
+          {date.toDateString()}
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default App;
